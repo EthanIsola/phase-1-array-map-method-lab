40e030a8407b0663a1b0a25ac3b217826bd9626a
Ethan Isola <isolaethan@gmail.com>
2022-01-12 09:48:10 -0600

    commit tree 89cd82f76d6afd3e3c70dc828504471487021304

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
+
 const titleCased = () => {
-  return tutorials
+
+  function capper(string){
+    let words = string.split(' ');
+    for(let i = 0; i < words.length; i++){
+      words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+    }
+    return words.join(' ');
+  }
+
+
+  return tutorials.map(capper);
 }
